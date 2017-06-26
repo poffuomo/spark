a35a07d5941ac414bc6e5c505b584e669a80c4b8
Manfredi Giordano <manfredi.giordano@me.com>
2017-06-26 16:10:53 +0200

    Added a new GraphX example class
    Connected Components algorithm to be applied to the LiveJournal social
    graph (https://snap.stanford.edu/data/com-LiveJournal.html).

---
+++ examples/src/main/scala/org/apache/spark/examples/graphx/LiveJournalConnectedComponents.scala
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// scalastyle:off println
+package org.apache.spark.examples.graphx
+
+/**
+ * Example of a Connected Components algorithm to execute on the `LiveJournal` dataset.
+ * Download the dataset from https://snap.stanford.edu/data/com-LiveJournal.html.
+ *
+ * @author poffuomo
+ * @version 1.0 6/26/17
+ */
+object LiveJournalConnectedComponents {
+  def main(args: Array[String]): Unit = {
+    if (args.length < 1) {
+      System.err.println(
+        "Usage: LiveJournalConnectedComponents <edge_list_file>\n" +
+          "    --numEPart=<num_edge_partitions>\n" +
+          "        The number of partitions for the graph's edge RDD.\n" +
+          "    [--tol=<tolerance>]\n" +
+          "        The tolerance allowed at convergence (smaller => more accurate). Default is " +
+          "0.001.\n" +
+          "    [--output=<output_file>]\n" +
+          "        If specified, the file to write the ranks to.\n" +
+          "    [--partStrategy=RandomVertexCut | EdgePartition1D | EdgePartition2D | " +
+          "CanonicalRandomVertexCut]\n" +
+          "        The way edges are assigned to edge partitions. Default is RandomVertexCut.")
+      System.exit(-1)
+    }
+
+    Analytics.main(args.patch(0, List("cc"), 0))
+  }
+}
+// scalastyle:on println
